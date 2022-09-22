4ed62d6a41f71c507dff705f31ddc432adc7d865
Jiahao Si <jiahao.si@shopee.com>
2022-09-22 18:57:50 +0800

    add chain comment

--- addChain.js
+++ addChain.js
@@ -5,7 +5,7 @@ function add(n) {
     return add(n + x);
   };
 
-  addNext.valueOf = function () {
+  addNext.toString = function () {
     return n;
   };
 
@@ -17,4 +17,20 @@ console.log(add(1)(2) == 3);
 console.log(add(1)(2)(3) == 6);
 console.log(typeof add(1)(2) === 'function');
 
-// 必须使用  == ，因为在运行 == 时，会进行强制类型转换，调用 valueOf
+// 必须使用  == ，因为在运行 == 时，会进行强制类型转换，调用 valueOf 或 toString， 如果是 字符的类型转化，则
+
+// class A {
+//   valueOf() {
+//       return 2
+//   }
+//   toString() {
+//       return '哈哈哈'
+//   }
+// }
+// let a = new A()
+
+// console.log(String(a))  // '哈哈哈'   => (toString)
+// console.log(Number(a))  // 2         => (valueOf)
+// console.log(a + '22')   // '222'     => (valueOf)
+// console.log(a == 2)     // true      => (valueOf)
+// console.log(a === 2)    // false     => (严格等于不会触发隐式转换)
